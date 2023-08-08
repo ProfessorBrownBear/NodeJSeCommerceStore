11a488a37acc661fe520ae37b0616eb3f7465916
Peter Sigurdson <139134226+ProfessorBrownBear@users.noreply.github.com>
2023-08-08 17:27:42 -0400

    Create generateStarterData.js

---
+++ generateStarterData.js
@@ -0,0 +1,24 @@
+// generateStarterData.js
+const mongoose = require('mongoose');
+const Product = require('./model'); // Importing the Product model
+
+mongoose.connect('mongodb://127.0.0.1:27017/mensWearDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  // Use the Product model here
+  const product = new Product({
+    name: 'Shirt',
+    price: 29.99,
+    category: 'Tops',
+    inStock: true
+  });
+
+  product.save().then(() => {
+    console.log('Product saved');
+    mongoose.connection.close();
+  });
+})
+.catch(err => console.error('Connection error', err));
